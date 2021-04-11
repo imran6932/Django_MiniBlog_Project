0be1d392ba44da96a1257988bd1e98604e2e8b3e
Imran Ansari <imransxcr53@gmail.com>
2021-04-11 20:54:25 +0530

    1st django project

---
+++ blog/views.py
@@ -0,0 +1,116 @@
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import SignupForm, LoginForm, PostForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .models import Post
+from django.contrib.auth.models import Group
+
+# Home
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts':posts})
+
+# About
+def about(request):
+    return render(request, 'blog/about.html' )
+
+
+# Contact
+def contact(request):
+    return render(request, 'blog/contact.html' )
+
+# dashboard
+def dashboard(request):
+    if request.user.is_authenticated:
+        posts = Post.objects.all()
+        name = request.user.get_full_name()
+        return render(request, 'blog/dashboard.html', {'posts':posts, 'full_name':name})
+    else:
+        return HttpResponseRedirect('/login/')
+
+# login
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = LoginForm(request=request, data=request.POST)
+            if form.is_valid():
+                uname = form.cleaned_data['username']
+                upass = form.cleaned_data['password']
+                user = authenticate(username=uname, password=upass)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'you have logged in successfully')
+                    return HttpResponseRedirect('/dashboard/')
+        else:
+            form = LoginForm()
+        return render(request, 'blog/login.html', {'form':form})
+    else:
+        return HttpResponseRedirect('/dashboard/')
+
+
+
+# logout
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'You have logged out successfully')
+    return HttpResponseRedirect('/')
+
+# signup
+def user_signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account created successfully')
+            form = SignupForm()
+            
+    else:
+        form = SignupForm()
+    return render(request, 'blog/signup.html', {'form':form})
+
+
+# Add Post
+def add_post(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = PostForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Your post has posted successfully')
+                form = PostForm()
+        else:
+            form = PostForm()
+        return render(request, 'blog/addpost.html', {'form':form})
+    else:
+        return HttpResponseRedirect('/login/')
+
+# Update Post
+def update_post(request,id):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            pi = Post.objects.get(pk=id)
+            form = PostForm(request.POST,instance=pi)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Post Updated successfully')
+                
+        else:
+            pi = Post.objects.get(pk=id)
+            form = PostForm(instance=pi)
+        return render(request, 'blog/updatepost.html', {'form':form})
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+# Delete Post
+def delete_post(request,id):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            pi = Post.objects.get(pk=id)
+            pi.delete()
+            messages.warning(request, 'Post Deleted Successfully.')
+        return HttpResponseRedirect('/dashboard/')
+    else:
+        return HttpResponseRedirect('/login/')
+    
+    
